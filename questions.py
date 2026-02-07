dba3207b861cf0788f40b5a2882eb25e79599d20
Lun 3 <lun.wo.16@gmail.com>
2026-02-07 17:48:02 +0500

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,71 @@
+# здесь храняться все вопросы для квизов
+QUESTIONS = {
+    "continuous": [
+    { # 1
+        "question": 'I ... (draw) you, Rose. (Из фильма "Титаник")',
+        "options": ["am drawing", "was drawing", "will be drawing"],
+        "correct": "am drawing",  # правильный ответ
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"  # подсказка для этого вопроса
+    },
+    { # 2
+        "question": 'We ... (move) to a new apartment tomorrow! (Из сериала "Друзья")',
+        "options": ["are moving", "were moving", "will be moving"],
+        "correct": "are moving",
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"  # подсказка для этого вопроса
+    },
+    { # 3
+        "question": 'You ... (break) school rules right now! (Из книги "Гарри Поттер и философский камень")',
+        "options": ["are breaking", "were breaking", "will be breaking"],
+        "correct": "are breaking",
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"  # подсказка для этого вопроса
+    },
+    { # 4
+        "question": 'He ... (chase) me through the streets last night. (Из фильма "Терминатор")',
+        "options": ["is chasing", "was chasing", "will be chasing"],
+        "correct": "was chasing",
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"  # подсказка для этого вопроса
+    },
+    { # 5
+        "question": 'I ... (meet) the CEO at 3 PM today. (Из сериала "Офис")',
+        "options": ["are meeting", "were meeting", "will be meeting"],
+        "correct": "are meeting",
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"  # подсказка для этого вопроса
+    },
+    { # 6
+        "question": 'Sauron ... (watch) us from Barad-dûr. (Из книги "Властелин колец")',
+        "options": ["is watching", "was watching", " will be watching"],
+        "correct": "is watching",
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"
+    },
+    { # 7
+        "question": 'You ... (live) in a simulation when I found you (Из фильма "Матрица")',
+        "options": ["are living", "were living", "will be living"],
+        "correct": "were living",
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"  # подсказка \ поменять
+    },
+    { # 8
+        "question": 'Sherlock ... (solve) the case at this very moment. (Из сериала "Шерлок")',
+        "options": ["is solving", "was solving", "will be solving"],
+        "correct": "is solving",
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"  # подсказка для этого вопроса
+    },
+    { # 9
+        "question": 'They ... (monitor) my thoughts even now. (Из книги "1984")',
+        "options": ["are monitoring", "were monitoring", "will be monitoring"],
+        "correct": "are monitoring",
+        "help": "Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"  # подсказка для этого вопроса
+    },
+    { # 10
+        "question": 'I ... fighting Darth Vader tomorrow. (Из фильма "Звёздные войны")',
+        "options": ["am fighting", "was fighting", "will be fighting"],
+        "correct": "will be fighting",
+        "help": ["Подлежащее + am/is/are (I + am, He/She/It + is, They/We/You + are) + глагол + -ing"]  # подсказка \ поменять
+    },
+    ]
+    ,
+    "tenses": [{"question": "Данный раздел ещё не заполнен :(",
+            "options": ["Подождать (схавать как терпила)", "Послать автора нахуй", "проигнорить", "жить дальше"],
+            "correct": "Послать автора нахуй",
+            "help": "Подумай ещё."}]
+    # Добавьте другие темы по аналогии
+}
